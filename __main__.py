5d67e1aace98d407f2947538f090f4763b7f4452
Damian Bourdin <damian.bourdin@gmail.com>
2019-09-24 22:48:08 -0300

    Added main.

---
+++ __main__.py
@@ -0,0 +1,12 @@
+"""
+flask-postgres.__main__
+~~~~~~~~~~~~
+
+Allows running flask-postgres from the command line.
+"""
+
+from server import start_server
+
+
+if __name__ == '__main__':
+    start_server()
