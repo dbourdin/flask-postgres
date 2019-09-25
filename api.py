a35522882ee78e952f31cc2f6b6f70f6bba0277e
Damian Bourdin <damian.bourdin@gmail.com>
2019-09-25 01:11:52 -0300

    Updated blueprint name.

--- api.py
+++ api.py
@@ -5,4 +5,4 @@ import config
 
 logger = logbook.Logger('[API]', getattr(logbook, config.LOGLEVEL))
 
-blueprint = Blueprint('api', __name__)
+users_blueprint = Blueprint('users', __name__, url_prefix='/users')
