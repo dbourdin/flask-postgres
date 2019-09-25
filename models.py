8c3b078aeca40690984564d3cde7643fee9df5f2
Damian Bourdin <damian.bourdin@gmail.com>
2019-09-25 01:03:06 -0300

    Using sql alchemy and marshmallow. Updated user model and created userschema.

--- models.py
+++ models.py
@@ -1,12 +1,12 @@
-from sqlalchemy import Column, Integer, String
-from database import Base
+from server import db
+from server import ma
 
 
-class User(Base):
+class User(db.Model):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
-    email = Column(String(120), unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    email = db.Column(db.String(120), unique=True)
 
     def __init__(self, name=None, email=None):
         self.name = name
@@ -14,3 +14,8 @@ class User(Base):
 
     def __repr__(self):
         return f'<User {self.name}>'
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'email')
