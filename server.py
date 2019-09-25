9f5235bc300a42da0b26a68555971872b38c699c
Damian Bourdin <damian.bourdin@gmail.com>
2019-09-24 22:47:20 -0300

    Added default server.py.

---
+++ server.py
@@ -0,0 +1,51 @@
+import logging
+import pathlib
+import signal
+import sys
+
+from flask import Flask
+from flask_cors import CORS
+from gevent.pywsgi import LoggingLogAdapter
+from gevent.pywsgi import WSGIServer
+import logbook
+
+import config
+
+# Create log directory
+pathlib.Path(config.LOG_PATH).mkdir(parents=True, exist_ok=True)
+
+logbook.StreamHandler(sys.stdout).push_application()
+logbook.RotatingFileHandler(config.LOGFILE, max_size=52428800,
+                            bubble=True).push_application()
+logbook.compat.redirect_logging(set_root_logger_level=False)
+logging.root.setLevel(config.LOGLEVEL)
+
+logger = logbook.Logger('[SERVER]', getattr(logbook, config.LOGLEVEL))
+
+app = Flask('flask')
+app.register_blueprint(blueprint)
+CORS(app)
+
+wsgi_logger = LoggingLogAdapter(logging.getLogger('wsgi'), level=logging.DEBUG)
+wsgi_server = WSGIServer((config.API_IP, config.API_PORT), app,
+                         log=wsgi_logger, error_log=wsgi_logger)
+
+
+def signal_handler(signal, frame):
+    logger.info('Exiting...')
+    wsgi_server.stop()
+
+
+def _register_signal_handler():
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.siginterrupt(signal.SIGINT, False)
+    signal.signal(signal.SIGTERM, signal_handler)
+    signal.siginterrupt(signal.SIGTERM, False)
+
+
+def start_server():
+    logger.info('Started')
+    _register_signal_handler()
+    wsgi_server.serve_forever()
+    logger.info('Exited')
+
